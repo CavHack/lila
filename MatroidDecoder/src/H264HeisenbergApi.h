dca2675021f032c246a2281886c660289c758252
Draco Cavalier <karl@blackesquirel.com>
2016-12-07 12:23:05 -0800

    Work on enumerations and structures for Decoding
    Updated for h264Dec2016 standards

---
+++ MatroidDecoder/src/H264HeisenbergApi.h
@@ -0,0 +1,12 @@
+/*------------------------------------------------------------------------------
+    Table of contents
+    1. Include Headers
+    2. Enumerations used as a return value or a parameter.
+        2.1. API's return value enumerations.
+    3. User Structures
+        3.1. Structures for H264SwDecDecode() parameters.
+        3.2. Structures for information interchange with
+             DEC API and user application.
+    4. Prototypes of Decoder API functions
+------------------------------------------------------------------------------*/
+
